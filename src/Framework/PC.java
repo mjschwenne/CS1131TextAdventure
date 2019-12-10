0bbb6592c92a83c4bc0759ebd52f1739a3bfc5ad
klink135 <mjjorgensen17@gmail.com>
2019-12-10 15:20:43 -0500

    PC and TechAdventure made mostly functional. There are TODO or comments in areas that still need attention.

--- src/Framework/PC.java
+++ src/Framework/PC.java
@@ -1,23 +1,66 @@
 package Framework;
+import java.util.ArrayList;
 
 public class PC {
-
     private Room currentRoom;
+    private ArrayList<Item> inventory = null;
 
-    public void getItem(){
-
+    public PC() {
+        this.inventory = new ArrayList<>();
     }
 
-    public void dropItem(){
+    public void getItem(String item){
+        inventory.add(new Item(item));
+    }
 
+    public void dropItem(String item){
+        //TODO Fix this thing.
+        inventory.remove(new Item(item));
     }
 
     public void look() {
-
+        //TODO this might change
+        System.out.println(currentRoom.getDescription(this));
     }
 
     public void printInventory(){
+        System.out.println(inventory);
+    }
 
+    public void go(String[] direction) {
+        if(direction.length > 2) {
+            //Portals
+            switch(direction[2]) {
+                case "NORTH":
+                    currentRoom = currentRoom.getPortalNorth();
+                    break;
+                case "EAST":
+                    currentRoom = currentRoom.getPortalEast();
+                    break;
+                case "WEST":
+                    currentRoom = currentRoom.getPortalWest();
+                    break;
+                case "SOUTH":
+                    currentRoom = currentRoom.getPortalSouth();
+                    break;
+            }
+        } else {
+            //Doors
+            switch(direction[1]) {
+                case"NORTH":
+                    currentRoom = currentRoom.getNorth();
+                    break;
+                case "EAST":
+                    currentRoom = currentRoom.getEast();
+                    break;
+                case "WEST":
+                    currentRoom = currentRoom.getWest();
+                    break;
+                case "SOUTH":
+                    currentRoom = currentRoom.getSouth();
+                    break;
+            }
+        }
     }
 
 }
--- src/TechAdventure.java
+++ src/TechAdventure.java
@@ -1,101 +1,78 @@
-import java.util.ArrayList;
+import Framework.PC;
 
-public class TechAdventure {
-    ArrayList<String> invArrayList = new ArrayList<>(); // temporary array list that represents your inventory
-    AdventureServer as = new AdventureServer;
+import java.io.IOException;
+
+public class TechAdventure implements ConnectionListener{
+    AdventureServer adventureServer = null;
+    PC player = null;
+
+    public TechAdventure() {
+        adventureServer = new AdventureServer ( );
+        player = new PC();
+        adventureServer.setOnTransmission ( this );
+    }
+
     public static void main(String[] args) {
-        AdventureServer server = new AdventureServer();
-        server.setOnTransmission(e -> System.out.println(e.getData()));
-        if(args.length >= 1) {
-            server.startServer(Integer.parseInt(args[0]));
+        TechAdventure techAdventure = new TechAdventure ();
+        if(args.length > 0) {
+            techAdventure.start(Integer.parseInt(args[0]));
         } else {
-            server.startServer(2112);
+            techAdventure.start ( 2112 );
         }
 
     }
 
-    public void getItem(String item) { // picks up items by checking to see if you have the item, checking if the item exists
-        boolean itemInInventory = false;
-        for ( int i = 0; i < invArrayList.size(); i++ ) { // checks to make sure that the item isn't already in your inventory
-            if ( item == invArrayList.get(i)) {
-                itemInInventory = true;
+    public void start( int port ) {
+        adventureServer.startServer ( port );
+    }
+
+    @Override
+    public void handle(ConnectionEvent e) {
+        //System.out.println( "EVENT RECEIVED - YOU MUST PARSE THE DATA AND RESPOND APPROPRIATELY");
+        //System.out.println( String.format ( "connectionId=%d, data=%s", e.getConnectionID (), e.getData() ));
+        try {
+            switch ( e.getCode ( ) ) {
+                case CONNECTION_ESTABLISHED:
+                    // What do you do when the connection is established?
+                    break;
+                case TRANSMISSION_RECEIVED:
+                    adventureServer.sendMessage ( e.getConnectionID ( ), String.format ("MESSAGE RECEIVED: connectionId=%d, data=%s", e.getConnectionID ( ), e.getData ( ) ) );
+                    switch(e.getData().toUpperCase().split(" ")[0]) {
+                        //GET, DROP, GO, LOOK, INVENTORY, SAVE, RESTORE, QUIT
+                        case "GET":
+                            player.getItem(e.getData().toUpperCase().split(" ")[1]);
+                            break;
+                        case "DROP":
+                            player.dropItem(e.getData().toUpperCase().split(" ")[1]);
+                            break;
+                        case "GO":
+                            player.go(e.getData().toUpperCase().split(" "));
+                            break;
+                        case "LOOK":
+                            player.look();
+                            break;
+                        case "INVENTORY":
+                            player.printInventory();
+                            break;
+                        case "SAVE":
+                            //Save here
+                            break;
+                        case "RESTORE":
+                            //Load save here
+                            break;
+                        case "QUIT":
+                            adventureServer.disconnect(e.getConnectionID());
+                            break;
+                    }
+                    break;
+                case CONNECTION_TERMINATED:
+                    // Cleanup when the connection is terminated.
+                    break;
+                default:
+                    // What is a reasonable default?
             }
+        } catch (UnknownConnectionException | IOException unknownConnectionException ) {
+            unknownConnectionException.printStackTrace ( );
         }
-
-        boolean itemExists = false;
-        for ( int i = 0; i < itemArrayList.size(); i++ ) { // checks to make sure that the item is actually an item in the game
-            if ( item == itemArrayList.get(i)) { // itemArrayList represents the list of real items in the game
-                itemExists = true;
-            }
-        }
-
-        if (itemInInventory == false && itemExists == true) { // if the item isn't in your inventory and the item exists, add it to your inventory
-            invArrayList.add(item);
-        } else if (itemExists == false) {
-            as.sendMessage(connectionId, "The item you entered doesn't exist (You entered " + item + ")");
-        } else if (itemInInventory == true) {
-            as.sendMessage(connectionId, "The item you entered is already in your inventory, so you can't pick it up");
-        }
-    }
-
-    public void dropItem(String item) { // picks up items by checking to see if you have the item, checking if the item exists
-        boolean itemInInventory = false;
-        for ( int i = 0; i < invArrayList.size(); i++ ) { // checks to make sure that the item is in your inventory
-            if ( item == invArrayList.get(i)) {
-                itemInInventory = true;
-            }
-        }
-
-        boolean itemExists = false;
-        for ( int i = 0; i < itemArrayList.size(); i++ ) { // checks to make sure that the item is actually an item in the game
-            if ( item == itemArrayList.get(i)) { // itemArrayList represents the list of real items in the game
-                itemExists = true;
-            }
-        }
-
-        if (itemInInventory == true && itemExists == true) { // if the item is in your inventory and the item exists, remove it to your inventory
-            invArrayList.remove(item);
-        } else if (itemExists == false) {
-            as.sendMessage(connectionId, "The item you entered doesn't exist (You entered " + item + ")");
-        } else if (itemInInventory == false) {
-            as.sendMessage(connectionId, "The item you entered isn't in your inventory, so you can't drop it");
-        }
-    }
-
-    public void goDirection(String direction) {
-        // if you can go in direction, go in direction
-    }
-
-    public String look() { // returns the current room description
-        return currentRoomDescription; // replace currentRoomDescription with the actual room description
-    }
-
-    public String look(String item) {
-        String lookString = "The specified item is not in your inventory"; // I set this up so if the item isnt in your inventory, this string is returned
-        for ( int i = 0; i < invArrayList.size(); i++ ) { // invArrayList is a temp name for the inventory array list
-            if ( item == invArrayList.get(i)) {
-                lookString = itemDescription; // replace itemDescription with the actual item description
-            }
-        }
-        return lookString;
-    }
-
-    public void inventory() {
-        as.sendMessage(connectionId, "The following items are in your inventory:");
-        for ( int i = 0; i < invArrayList.size(); i++ ) { // checks to make sure that the item is in your inventory
-            as.sendMessage(connectionId, invArrayList.get(i));
-        }
-    }
-
-    public void save() {
-
-    }
-
-    public void restore() {
-
-    }
-
-    public void quit() {
-        as.disconnect(connectionId);
     }
 }
