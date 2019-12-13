3a2a211ffc30766588de485e0e6a9ec9c150487f
klink135 <mjjorgensen17@gmail.com>
2019-12-12 19:07:03 -0500

    Finished combat system.

--- src/Framework/NPC.java
+++ src/Framework/NPC.java
@@ -3,47 +3,70 @@ package Framework;
 public class NPC {
 	private int x = 0;
 
-	private int getRandomNumber() {
-		return ( int )( Math.random( ) * ( ( 3 - 1 ) + 1 ) ) + 1;
+	public String getRandomWeapon() {
+		switch((int)( Math.random( ) * 2 + 1)) {
+			default:
+				return "KNIFE";
+			case 2:
+				return "BUBBLES";
+			case 3:
+				return "TORCH";
+		}
 	}
 
-	public String combat( String weapon) {
+	public String combat(PC pc, String weapon, String enemyWeapon) {
 		switch( weapon ) {
 			case "KNIFE":
-				x = getRandomNumber();
-				if ( x == 1 ) {
-					return "tie";
-				} else if ( x == 2 ) {
-					return "loss";
-				} else if ( x == 3 ) {
-					return "win";
+				if(enemyWeapon == "BUBBLES") {
+					pc.inCombat = false;
+					pc.beastSlain = true;
+					return "You quickly stab the beast.";
+				} else if(enemyWeapon == "TORCH") {
+					pc.inCombat = false;
+					pc.gameOver = true;
+					return "You have been burned to a crisp. Use RESET to restart or QUIT to disconnect.";
 				} else {
-					return null;
+					return "You both clash your weapons. You are in combat. Enter FIGHT to pick a weapon.";
 				}
 			case "TORCH":
-				x = getRandomNumber();
-				if ( x == 1 ) {
-					return "win";
-				} else if ( x == 2 ) {
-					return "tie";
-				} else if ( x == 3 ) {
-					return "loss";
+				if(enemyWeapon == "KNIFE") {
+					pc.inCombat = false;
+					pc.beastSlain = true;
+					return "You burned the beast to a crisp.";
+				} else if(enemyWeapon == "BUBBLES") {
+					pc.inCombat = false;
+					pc.gameOver = true;
+					return "Your light has been put out. Use RESET to restart or QUIT to disconnect.";
 				} else {
-					return null;
+					return "You both now have a bigger fire. You are in combat. Enter FIGHT to pick a weapon.";
 				}
 			case "BUBBLES":
-				x = getRandomNumber();
-				if ( x == 1 ) {
-					return "loss";
-				} else if ( x == 2 ) {
-					return "win";
-				} else if ( x == 3 ) {
-					return "tie";
+				if(enemyWeapon == "TORCH") {
+					pc.inCombat = false;
+					pc.beastSlain = true;
+					return "You dowsed the beast's flame.";
+				} else if(enemyWeapon == "KNIFE") {
+					pc.inCombat = false;
+					pc.gameOver = true;
+					return "The beast pops your bubble. Use RESET to restart or QUIT to disconnect.";
 				} else {
-					return null;
+					return "Your bubbles just bounce off each other.";
 				}
 			default:
 				return "Invalid Command";
+		}
+	}
+
+	public String monsterWeaponDialog(String weapon) {
+		switch (weapon) {
+			case "TORCH":
+				return "The Beast looks as if it wants you to burn.";
+			case "KNIFE":
+				return "The Beast wants to fillet you.";
+			case "BUBBLES":
+				return "The Beast wants you to float away";
+			default:
+				return "The Beast wants to kill you.";
 		}
 	}
 }
